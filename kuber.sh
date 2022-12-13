3f188ba381f70ba733ab1ecdd62b57ab3216a406
Juan Rodriguez <111403996+JuanDavRH@users.noreply.github.com>
2022-12-13 11:08:52 -0500

    Update kuber.sh

--- kuber.sh
+++ kuber.sh
@@ -1,6 +1,6 @@
 echo "kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo apt get update
+sudo apt update
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
